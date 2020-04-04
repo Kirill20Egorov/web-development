c209971f4a8c566cfd239aeff1e1df2a76021ce6
Kirill_Egorov <37872861+Kirill20Egorov@users.noreply.github.com>
2020-04-04 15:49:54 +0300

    Update script.js

--- lw7(task1)/script.js
+++ lw7(task1)/script.js
@@ -1,35 +1,35 @@
 function isPrimeNumber(num){
   isPrime = true
   if (typeof num == 'number'){
-	for (let j = 2; j < num; j++){
-	  if (num % j == 0){
-		isPrime = false;
-		break;
-	  }
-	}
-	if (isPrime){
+    for (let j = 2; j < num; j++){
+      if (num % j == 0){
+        isPrime = false;
+	break;
+      }
+    }
+    if (isPrime){
       console.log(num + ' is prime number')
     }
-	else{
-	  console.log(num + ' is not prime number')
-	}
+    else{
+      console.log(num + ' is not prime number')
+    }
   }
   else{
-	if (typeof num == 'object'){
-	  for (let index = 0; index < num.length; index++){
-		for (let j = 2; j < num[index]; j++){
-		  if (num[index] % j == 0){
-			isPrime = false;
-			break;
-		  }
-		}
-	    if (isPrime){
-		  console.log(num[index] + ' is prime number')
-        }
-		else{
-		  console.log(num[index] + ' is not prime number')
-		}
+    if (typeof num == 'object'){
+      for (let index = 0; index < num.length; index++){
+        for (let j = 2; j < num[index]; j++){
+	  if (num[index] % j == 0){
+	    isPrime = false;
+	    break;
 	  }
 	}
+        if (isPrime){
+          console.log(num[index] + ' is prime number')
+        }
+        else{
+	 console.log(num[index] + ' is not prime number')
+        }
+      }
+    }
   }
 }
