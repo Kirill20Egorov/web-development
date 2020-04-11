610dec3ba5277fd80c017afe483fe3971d26a006
Kirill_Egorov <37872861+Kirill20Egorov@users.noreply.github.com>
2020-04-11 18:44:53 +0300

    Update script.js

--- lw7(task1)/script.js
+++ lw7(task1)/script.js
@@ -8,7 +8,7 @@ function isPrimeNumber(num){
   if (typeof num == 'object'){
     for (let index = 0; index < num.length; index++){
       //чтобы проверить число на простое достаточно проверить делимость на 3 и на 2, вместо запуска цикла в больших количествах
-      if (((num[index] % 2 == 0) || (num[index] % 3 == 0)) && !((num[index] <= 3) && (num[index] >= -3))){
+      if ((((num[index] % 2 == 0) || (num[index] % 3 == 0)) && !((num[index] <= 3) && (num[index] >= -3))) || num[index] == 0)
         isPrime = false;
       }
       if (isPrime){
