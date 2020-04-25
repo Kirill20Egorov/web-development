43ad352e7aa273aa1389f035481f4ac9437f239f
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-04-25 17:37:14 +0300

    task1

--- lw7(task1)/script.js
+++ lw7(task1)/script.js
@@ -7,9 +7,6 @@ function isPrimeNumber(num){
   }
   if (typeof num == 'object'){
     for (let index = 0; index < num.length; index++){
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       isPrime = false;
       if (num[index] >= 2){
         isPrime = true; 
@@ -18,39 +15,14 @@ function isPrimeNumber(num){
             isPrime = false;
             break;
           } 
-        }     	
-=======
-=======
->>>>>>> 058c5424500df90e57e466db6c9356e84d0959be
-=======
->>>>>>> 058c5424500df90e57e466db6c9356e84d0959be
-      //чтобы проверить число на простое достаточно проверить делимость на 3 и на 2, вместо запуска цикла в больших количествах
-      if ((((num[index] % 2 == 0) || (num[index] % 3 == 0)) || (num[index] <= 1)) 
-      && num[index] != 2 && num[index] != 3){
-        isPrime = false;
->>>>>>> 058c5424500df90e57e466db6c9356e84d0959be
-      }
+        } 
+      }      
       if (isPrime){
         console.log(num[index] + ' is prime number')
       }
       else{
         console.log(num[index] + ' is not prime number')
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       }  
-=======
-      }
-      isPrime = true;
->>>>>>> 058c5424500df90e57e466db6c9356e84d0959be
-=======
-      }
-      isPrime = true;
->>>>>>> 058c5424500df90e57e466db6c9356e84d0959be
-=======
-      }
-      isPrime = true;
->>>>>>> 058c5424500df90e57e466db6c9356e84d0959be
     }
   }
 }
