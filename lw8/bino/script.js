25fb9b991e2a30dab33389e5edd06eb9bd0fc918
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-04-25 19:04:35 +0300

    fixed

--- lw8/bino/script.js
+++ lw8/bino/script.js
@@ -7,18 +7,18 @@ prev.onclick = function(){
   changeSlide(-1);    
 } 
 next.onclick = function(){
-  changeSlide(+1);    
+  changeSlide(1);    
 } 
 slide[currentSlide-1].style.display = "block";  
 function changeSlide(n){  
   currentSlide += n;
   if(currentSlide > slide.length){
-  	currentSlide = 1;
+    currentSlide = 1;
   }
   if(currentSlide < 1){
-  	currentSlide = slide.length;
-  }	
-  for (i = 0; i < slide.length; i++) {
+    currentSlide = slide.length;
+  }
+  for (i = 0; i < slide.length; i++){
     slide[i].style.display = "none";
   }
   slide[currentSlide-1].style.display = "block";  
